7ce616e482d5cf72ddc741760efb533526146709
Tiago <tgvieirabr@gmail.com>
2023-12-08 11:23:46 -0300

    first commit

---
+++ database/migrations/2023_12_08_133055_create_filme_streaming_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filme_streaming', function (Blueprint $table) {
+            $table->foreignId('filme_id')->constrained();
+            $table->foreignId('streaming_id')->constrained();
+                       
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filme_streaming');
+    }
+};
